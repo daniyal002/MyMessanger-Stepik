4e5920b9e0f627f4504b98a9b71dc0243f0bf3c1
IdrisovDSH <daniyalou@gmail.com>
2022-06-03 16:17:22 +0300

    Создание клиента

---
+++ ConsoleClient/MessengerClientAPI.cs
@@ -0,0 +1,63 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyMessanger_Stepik
+{
+    class MessengerClientAPI
+    {
+        public void TestNewtonsoftJson()
+        {
+            Message msg = new Message("Daniyal", "Hello", DateTime.UtcNow);
+            string output = JsonConvert.SerializeObject(msg);
+            Message dsmsg = JsonConvert.DeserializeObject<Message>(output);
+        }
+
+        public Message GetMessage(int MessageId)
+        {
+            WebRequest request = WebRequest.Create("http://localhost:5000/api/Messanger/" + MessageId.ToString());
+            request.Method = "Get";
+            WebResponse response = request.GetResponse();
+            string status = ((HttpWebResponse)response).StatusDescription;
+            Stream dataStream = response.GetResponseStream();
+            StreamReader streamReader = new StreamReader(dataStream);
+            string responseFromServer = streamReader.ReadToEnd();
+            streamReader.Close();
+            dataStream.Close();
+
+            if ((status.ToLower() == "ok") && (responseFromServer != "Not found"))
+            {
+                Message deserializedMsg = JsonConvert.DeserializeObject<Message>(responseFromServer);
+                return deserializedMsg;
+            }
+            return null;
+
+        }
+
+        public bool SendMessage(Message msg)
+        {
+            WebRequest request = WebRequest.Create("http://localhost:5000/api/Messanger/");
+            request.Method = "POST";
+            string postData = JsonConvert.SerializeObject(msg);
+            byte[] byteArray = Encoding.UTF8.GetBytes(postData);
+            request.ContentType = "application/json";
+            request.ContentLength = byteArray.Length;
+            Stream dataStream = request.GetRequestStream();
+            dataStream.Write(byteArray, 0, byteArray.Length);
+            dataStream.Close();
+            WebResponse response = request.GetResponse();
+            dataStream = response.GetResponseStream();
+            StreamReader reader = new StreamReader(dataStream);
+            string responseFromServer = reader.ReadToEnd();
+            reader.Close();
+            dataStream.Close();
+            response.Close();
+            return true;
+        }
+
+    }
+}
--- ConsoleClient/Program.cs
+++ ConsoleClient/Program.cs
@@ -4,13 +4,36 @@ namespace MyMessanger_Stepik
 {
     class Program
     {
+        private static int MessageID;
+        private static string UserName;
+        private static MessengerClientAPI API = new MessengerClientAPI();
+        private static void GetNewMessages()
+        {
+            Message msg = API.GetMessage(MessageID);
+            while (msg != null)
+            {
+                Console.WriteLine(msg);
+                MessageID++;
+                msg = API.GetMessage(MessageID);
+            }
+        }
+
         static void Main(string[] args)
         {
-            Message msg = new Message("Daniyal", "Hello", DateTime.UtcNow);
-            string output = JsonConvert.SerializeObject(msg);
-            Message dsmsg = JsonConvert.DeserializeObject<Message>(output);
-            Console.WriteLine(output);
-            Console.WriteLine(dsmsg);
+            MessageID = 1;
+            Console.WriteLine("Введите ваше имя");
+            UserName = Console.ReadLine();
+            string MessageText = "";
+            while (MessageText != "exit")
+            {
+                GetNewMessages();
+                MessageText = Console.ReadLine();
+                if (MessageText.Length > 1)
+                {
+                    Message Sendmsg = new Message(UserName, MessageText, DateTime.Now);
+                    API.SendMessage(Sendmsg);
+                }
+            }
         }
     }
 }
