f3452d236667e4f97b4e8a9d846365215fc8dff0
IdrisovDSH <daniyalou@gmail.com>
2022-06-03 14:06:48 +0300

    Создание Сериализатора/Десериализатора

--- ConsoleClient/Program.cs
+++ ConsoleClient/Program.cs
@@ -1,11 +1,16 @@
-﻿namespace MyMessanger_Stepik
+﻿using Newtonsoft.Json;
+
+namespace MyMessanger_Stepik
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Message msg = new Message();
-            Console.WriteLine(msg.ToString());
+            Message msg = new Message("Daniyal", "Hello", DateTime.UtcNow);
+            string output = JsonConvert.SerializeObject(msg);
+            Message dsmsg = JsonConvert.DeserializeObject<Message>(output);
+            Console.WriteLine(output);
+            Console.WriteLine(dsmsg);
         }
     }
 }
